c6428f082442a50f6a80ebfc65004367798f85c4
Jose Ramirez <jose.ramirez@ey.com>
2019-12-02 18:02:26 -0600

    Added new views and initial form for search component

--- my_app/views.py
+++ my_app/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'base.html')
 def new_search(request):
-    return render(request, 'base.html')
+    return render(request, 'my_app/new_search.html')
